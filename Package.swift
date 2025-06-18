09303cfb3d674e029c27ea437b0d27f0d0aa8cc0
Jordan Morgan <the_morgan_fam@yahoo.com>
2025-06-18 15:12:28 -0500

    Switched up structure

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIBackports",
-    platforms: [.iOS(.v26)],
+    platforms: [.iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -20,6 +20,5 @@ let package = Package(
             name: "SwiftUIBackports",
             path: "Sources/SwiftUIBackports"
         ),
-
     ]
 )
